75b3576e600f4cf2dd8a3d7df119c798a9b4cd82
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-31 01:51:17 +0600

    product details modal open

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ const displayPhones = (phones, dataLimit) => {
             <h5 class="card-title">${phone.phone_name}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 
-            <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
+            <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
              
           </div>
         </div>
@@ -62,7 +62,7 @@ const displayPhones = (phones, dataLimit) => {
 
 // search input  field enter key handler
 document.getElementById('search-field').addEventListener('keypress' , function(e){
-    console.log(e.key);
+    // console.log(e.key);
     if(e.key === 'Enter'){
        processSearch();
     }
@@ -108,7 +108,23 @@ document.getElementById('btn-show-all').addEventListener('click' , function(){
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    displayPhoneDetails(data.data);
   }
 
-// loadPhones();
+
+ const displayPhoneDetails = phone => {
+     console.log(phone);
+     const modalTitle = document.getElementById('phoneDetailModalLabel');
+     modalTitle.innerText = phone.name;
+     const phoneDetails = document.getElementById('phone-details');
+     phoneDetails.innerHTML = `
+     
+        <p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'No Release Date Found'}</p>
+        <p>Storage: ${phone.mainFeatures ? phone.mainFeatures.storage : 'No Storage Information '}</p>
+            <p>Others: ${phone.others ? phone.others.Bluetooth : 'No Bluetooth Information'}</p>
+            <p>Sensor: ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors[0] : 'no sensor'}</p>
+
+     
+     `;
+ } 
+loadPhones('apple');
